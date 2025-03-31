2ca59e3a68799eb907b2389b475c65866d181050
frank0224 <frank@shopline.com>
2025-04-01 01:21:30 +0800

    init

---
+++ spec/mongory_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Mongory do
+  it "has a version number" do
+    expect(Mongory::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
