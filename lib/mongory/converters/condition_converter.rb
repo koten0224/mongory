1a5eeede26cb9310e07efabf63d525875d5c0af2
frank0224 <frank@shopline.com>
2025-04-09 21:07:38 +0800

    docs(converter): add documentation for condition_converter

--- lib/mongory/converters/condition_converter.rb
+++ lib/mongory/converters/condition_converter.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 module Mongory
-  # Temp Description
+  # Converts flat query conditions into nested hash structure.
+  #
+  # Used by QueryBuilder to normalize condition input for internal matching.
+  #
+  # Combines key transformation (via KeyConverter) and
+  # value normalization (via ValueConverter), and merges overlapping keys.
+  #
+  # @example
+  #   ConditionConverter.convert({ "foo.bar" => 1, "foo.baz" => 2 })
+  #   # => { "foo" => { "bar" => 1, "baz" => 2 } }
   module Converters
     ConditionConverter = Object.new
     ConditionConverter.instance_eval do
+      # Converts a flat condition hash into a nested structure.
+      #
+      # @param condition [Hash]
+      # @return [Hash] nested condition
       def convert(condition)
         result = {}
         condition.each_pair do |k, v|
@@ -12,10 +25,12 @@ module Mongory
           converted_pair = key_converter.convert(k, converted_value)
           result.merge!(converted_pair, &deep_merge_block)
         end
-
         result
       end
 
+      # Deep merge block used to merge nested hashes
+      #
+      # @return [Proc]
       def deep_merge_block
         @deep_merge_block ||= Proc.new do |_, a, b|
           if a.is_a?(Hash) && b.is_a?(Hash)
@@ -26,14 +41,23 @@ module Mongory
         end
       end
 
+      # Returns the key converter
+      #
+      # @return [ConverterBuilder]
       def key_converter
         KeyConverter
       end
 
+      # Returns the value converter
+      #
+      # @return [ConverterBuilder]
       def value_converter
         ValueConverter
       end
 
+      # Prepares and freezes all internal components
+      #
+      # @return [ConditionConverter]
       def freeze
         deep_merge_block
         super
