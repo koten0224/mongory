ff4d1d7f9e62d6c033f7499ab1c1796ee4d1e7d2
frank0224 <frank@shopline.com>
2025-04-11 03:55:07 +0800

    docs: add documentation for or_matcher

--- lib/mongory/matchers/or_matcher.rb
+++ lib/mongory/matchers/or_matcher.rb
@@ -21,9 +21,9 @@ module Mongory
     #
     # @see AbstractMultiMatcher
     class OrMatcher < AbstractMultiMatcher
+      dispatch!
       # Constructs a ConditionMatcher for each subcondition.
       # Conversion is disabled to avoid double-processing.
-      dispatch!
 
       # @see ConditionMatcher
       # @param condition [Object] a subcondition to be wrapped
@@ -39,9 +39,9 @@ module Mongory
         :any?
       end
 
-      # Ensures the condition is an array of subconditions.
+      # Ensures the condition is an array of hashes.
       #
-      # @raise [TypeError] if condition is not an array
+      # @raise [Mongory::TypeError] if not valid
       # @return [void]
       def check_validity!
         raise TypeError, '$or needs an array' unless @condition.is_a?(Array)
